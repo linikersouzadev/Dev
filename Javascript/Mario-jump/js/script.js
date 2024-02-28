69f74e6d4ab042ec89620975c69f93d57f81318a
linikersouza <linikersouzadev>
2024-02-27 21:23:05 -0300

    imagens e scripts adicionados

--- Javascript/Mario-jump/js/script.js
+++ Javascript/Mario-jump/js/script.js
@@ -1,1 +1,13 @@
-XMLDocument
+const mario = document.querySelector('.mario');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+
+    },500);
+}    
+
+document.addEventListener('keydown', jump);
